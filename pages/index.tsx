e905b44cb12d55e3050ddf831cc351eed61c1176
pilyav4ik <ipiliavskyi@gmail.com>
2023-04-02 19:15:42 +0200

    #2 Image configuration.  Added spacial offer block

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import Header from "../components/screens/header/Header";
+import Offer from "../components/screens/offer/Offer";
 
 
 export default function Home() {
     return (
         <>
             <Header />
+            <Offer />
         </>
     )
 }
